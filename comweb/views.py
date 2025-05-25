5edc6c694a5a5ce6194b5f0f5b1a0e9f96288e7e
Jana Hamdy <jabdelma@andrew.cmu.edu>
2025-05-25 15:46:50 +0300

    added context to home.html

--- comweb/views.py
+++ comweb/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from comweb.models import *
 def home(request): 
-    return render(request, "comweb/home.html")
+    context = {
+        "modes": [x.NA for x in Mode.objects.all()],
+        "machine_types": [x.NA for x in MachineType.objects.all()],
+        "machines": [x.NA for x in Machine.objects.all()],
+        "resources": [x.NA for x in Resource.objects.all()],
+        "problem_types": [x.NA for x in ProblemType.objects.all()],
+        "bounds": [x.NA for x in Bound.objects.all()],
+        "classes": [x.NA for x in Class.objects.all()]
+    }
+    return render(request, "comweb/home.html", context)
 
