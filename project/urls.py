5bd5c420fb4f7e42ece12839a0466f157ae09b7f
Jana Hamdy <jabdelma@andrew.cmu.edu>
2025-05-26 07:59:07 +0300

    added static url pattern

--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from comweb.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name="home"),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
