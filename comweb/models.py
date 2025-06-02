cfa8efd9d92b58fe98e13d1ffdd513223e02e797
Jana Hamdy <jabdelma@andrew.cmu.edu>
2025-06-02 11:40:23 +0300

    added mmg, mtg tables

--- comweb/models.py
+++ comweb/models.py
@@ -63,9 +63,33 @@ class Method(models.Model):
     class Meta:
         ordering = ['SO']
 
+# machine type generalization 
+class MTG(models.Model):
+    lower = models.ForeignKey(MachineType, on_delete=models.CASCADE, related_name='lower_machine_types')
+    upper = models.ForeignKey(MachineType, on_delete=models.CASCADE, related_name='upper_machine_types')
+    method = models.CharField(max_length=100) # manually added or by transitive closure
+    row1 = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='mtg_row1_set') # if this is a transitive closure, then row1 and row2 will point to the MTG instances that were used to deduce this instance
+    row2 = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='mtg_row2_set')
+
+class ManualMTG(models.Model):
+    lower = models.ForeignKey(MachineType, on_delete=models.CASCADE, related_name='manual_lower_machine_types')
+    upper = models.ForeignKey(MachineType, on_delete=models.CASCADE, related_name='manual_upper_machine_types')
+    justification = models.CharField(max_length=500) # justification for why lower machine type is a generalization of upper machine type
+
+# machine mode generalization
+class MMG(models.Model):
+    lower = models.ForeignKey(MachineMode, on_delete=models.CASCADE, related_name='lower_machine_modes')
+    upper = models.ForeignKey(MachineMode, on_delete=models.CASCADE, related_name='upper_machine_modes')
+    method = models.CharField(max_length=100) # manually added or by transitive closure
+    row1 = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='mmg_row1_set')
+    row2 = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='mmg_row2_set')
+
+class ManualMMG(models.Model):
+    lower = models.ForeignKey(MachineMode, on_delete=models.CASCADE, related_name='manual_lower_machine_modes')
+    upper = models.ForeignKey(MachineMode, on_delete=models.CASCADE, related_name='manual_upper_machine_modes')
+    justification = models.CharField(max_length=500) # justification for why lower machine mode is a generalization of upper machine mode
+
 class AutoInclusion(models.Model):
     lower = models.ForeignKey(Class, on_delete=models.CASCADE, related_name='lower_classes')
     upper = models.ForeignKey(Class, on_delete=models.CASCADE, related_name='upper_classes')
     method = models.ForeignKey(Method, on_delete=models.PROTECT, related_name='auto_inclusions')
-
-
