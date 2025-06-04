e5872a274d2a0b4abeeb13559f9df875800e77f5
Jana Hamdy <jabdelma@andrew.cmu.edu>
2025-06-04 09:50:07 +0300

    added ajax

--- comweb/views.py
+++ comweb/views.py
@@ -1,20 +1,73 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from comweb.models import *
+
 def home(request): 
-    context = {
-        "modes": [x.NA for x in MachineMode.objects.all()],
-        "machine_types": [x.NA for x in MachineType.objects.all()],
-        "machines": [x.NA for x in Machine.objects.all()],
-        "resources": [x.NA for x in Resource.objects.all()],
-        "problem_types": [x.NA for x in ProblemType.objects.all()],
-        "bounds": [x.NA for x in ResourceBound.objects.all()],
-        "classes": [x.NA for x in Class.objects.all()], 
-        "methods": [x.NA for x in Method.objects.all()],
-        "inclusions": AutoInclusion.objects.all(),
-        "manual_mtgs": ManualMTG.objects.all(),
-        "mtgs": MTG.objects.all(),
-        "manual_mmgs": ManualMMG.objects.all(),
-        "mmgs": MMG.objects.all(),
-    }
-    return render(request, "comweb/home.html", context)
+    print("Home view accessed")
+    return render(request, "comweb/home.html")
+
+def get_list(request, list_type):
+    data = []
+    if list_type == 'modesList':
+        data = [x.NA for x in Mode.objects.all()]
+    elif list_type == 'typesList':
+        data = [x.NA for x in MachineType.objects.all()]
+    elif list_type == 'machinesList':
+        data = [x.NA for x in Machine.objects.all()]
+    elif list_type == 'resourcesList':
+        data = [x.NA for x in Resource.objects.all()]
+    elif list_type == 'problemTypesList':
+        data = [x.NA for x in ProblemType.objects.all()]
+    elif list_type == 'boundsList':
+        data = [x.NA for x in ResourceBound.objects.all()]
+    elif list_type == 'classesList':
+        data = [x.NA for x in Class.objects.all()]
+    elif list_type == 'inclusionsList':
+        data = [
+            {
+                'lower': x.lower.AB if hasattr(x.lower, 'AB') else '',
+                'upper': x.upper.AB if hasattr(x.upper, 'AB') else ''
+            }
+            for x in AutoInclusion.objects.all()
+        ]
+    elif list_type == 'methodsList':
+        data = [
+            {'name': x.NA, 'description': getattr(x, 'description', '')}
+            for x in Method.objects.all()
+        ]
+    elif list_type == 'manualMTGList':
+        data = [
+            {
+                'lower': x.lower.NA if hasattr(x.lower, 'NA') else '',
+                'upper': x.upper.NA if hasattr(x.upper, 'NA') else '',
+                'justification': x.justification
+            }
+            for x in ManualMTG.objects.all()
+        ]
+    elif list_type == 'MTGList':
+        data = [
+            {
+                'lower': x.lower.NA if hasattr(x.lower, 'NA') else '',
+                'upper': x.upper.NA if hasattr(x.upper, 'NA') else ''
+            }
+            for x in MTG.objects.all()
+        ]
+    elif list_type == 'manualMMGList':
+        data = [
+            {
+                'lower': x.lower.NA if hasattr(x.lower, 'NA') else '',
+                'upper': x.upper.NA if hasattr(x.upper, 'NA') else '',
+                'justification': x.justification
+            }
+            for x in ManualMMG.objects.all()
+        ]
+    elif list_type == 'MMGList':
+        data = [
+            {
+                'lower': x.lower.NA if hasattr(x.lower, 'NA') else '',
+                'upper': x.upper.NA if hasattr(x.upper, 'NA') else ''
+            }
+            for x in MMG.objects.all()
+        ]
+    return JsonResponse({'data': data})
 
