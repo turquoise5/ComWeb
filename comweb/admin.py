488de577c19cd6d96dc981360bb71c39c7045986
Jana Hamdy <jabdelma@andrew.cmu.edu>
2025-06-17 11:57:28 +0300

    added admin view

--- comweb/admin.py
+++ comweb/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Class, Machine, MachineMode, MachineType, ProblemType, ResourceBound, AutoInclusion, Method, ManualMTG, MMG, ManualMMG, MTG, ManualInclusion, Inclusion
 
-# Register your models here.
+admin.site.register(MachineMode)
+admin.site.register(MachineType)
+admin.site.register(Machine)
+admin.site.register(ProblemType)
+admin.site.register(ResourceBound)
+admin.site.register(Class)
+admin.site.register(Method)
+admin.site.register(MTG)
+admin.site.register(ManualMTG)
+admin.site.register(MMG)
+admin.site.register(ManualMMG)
+admin.site.register(AutoInclusion)
+admin.site.register(ManualInclusion)
+admin.site.register(Inclusion)
+
