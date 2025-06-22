e2c2bdd572ab623693490b5f516bb94e02841be3
Jana Hamdy <jabdelma@andrew.cmu.edu>
2025-06-23 00:47:30 +0300

    created ad added all co classes

--- comweb/management/commands/utils/class_populator.py
+++ comweb/management/commands/utils/class_populator.py
@@ -1,5 +1,5 @@
 from comweb.models import Class, ProblemType, ResourceBound
-from comweb.management.commands.class_data import get_class_data
+from comweb.management.commands.data.class_data import get_class_data
 
 def populate_classes(problem_type_data, bounds_data, machines):
     # Create basic types
@@ -12,3 +12,21 @@ def populate_classes(problem_type_data, bounds_data, machines):
 
     class_data = get_class_data(problem_types, bounds, machines)
     Class.objects.bulk_create([Class(**data) for data in class_data])
+    Class.objects.bulk_create([Class(
+        NA="co-" + data['NA'],
+        AB="co-" +data['AB'],
+        problem_type=data['problem_type'],
+        machine=data['machine'],
+        co=True,
+        co_class=Class.objects.get(AB=data['AB']),
+        time_bound=data.get('time_bound', bounds['inf']),
+        space_bound=data.get('space_bound', bounds['inf']),
+        alternations_bound=data.get('alternations_bound', bounds['inf'])
+    ) for data in class_data])
+
+    for c in Class.objects.all():
+        if not c.co:
+            c.co_class = Class.objects.get(AB="co-" + c.AB)
+            c.save()
+
+    return {c.AB: c for c in Class.objects.all()}
